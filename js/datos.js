d74ca12c1953c81bbf4a07bf49a2e58b1388eccd
sil8a <silviacatalina.ochoaes@elorrieta-errekamari.com>
2026-01-14 18:51:14 +0100

    ecructura de html y css vercion 1

---
+++ js/datos.js
@@ -0,0 +1,17 @@
+// Simulamos los usuarios de la base de datos
+const usuariosBD = [
+    { user: "admin", pass: "1234" },
+    { user: "cliente1", pass: "hola" }
+];
+
+// Simulamos las películas y sesiones
+const cinesBD = [
+    {
+        nombre: "Cine Elorrieta",
+        peliculas: [
+            { titulo: "Avatar 2", sesiones: ["16:00", "18:00", "20:00"], precio: 9.50 },
+            { titulo: "Super Mario", sesiones: ["17:00", "19:00"], precio: 8.00 }
+        ]
+    }
+];
+
