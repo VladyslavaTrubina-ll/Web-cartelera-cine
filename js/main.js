74dc53095821276fbf95c277431cb0743177d44d
VladyslavaTrubina-ll <vladyslava.trubina@elorrieta-errekamari.com>
2026-01-25 04:32:54 +0200

    anadir calcularPrecio()

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-import { peliculas, salas, sesiones, clientes, entradas } from "./db.js";
-import { generarID } from "./idGenerator.js";
-
-console.log("main cargado");
-
+export function calcularPrecio(cantidad, precioUnitario) {
+  if (cantidad >= 3) {
+    return cantidad * precioUnitario * (1 - 0.3); // 30% de descuento
+  }
+  if (cantidad == 2) {
+    return cantidad * precioUnitario * (1 - 0.2); // 20% de descuento
+  } else {
+    return cantidad * precioUnitario; // Sin descuento
+  }
+}
